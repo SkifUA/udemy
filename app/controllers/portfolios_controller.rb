53ef451d636c42ab4c5242c2e43ad95f32f2beac
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-08-22 12:45:58 +0300

    PorfoliosController integrated new and create actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,25 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios_items = Portfolio.all
   end
+
+  def new
+    @portfolios_item = Portfolio.new
+  end
+
+  def create
+    @portfolios_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolios_item.save
+        format.html { redirect_to portfolios_path, notice: "Your Profile Item added" }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
