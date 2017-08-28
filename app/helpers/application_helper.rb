9d9d05ff32e7600cfd861b9c2d755c587512bb17
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-08-28 14:40:44 +0300

    Updated styles for layout file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,6 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright('Kuzin Valeriy', 'All rights reserved')
+    DevcampViewTool::Renderer.copyright('Valerii Kuzin', 'All rights reserved')
   end
 end
