0e719f68c378de326c2f96d00a4d7687afcfc3dc
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-09-05 13:50:12 +0300

    Updatet file upload form

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,7 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -13,10 +12,4 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails_items, -> { where(subtitle: 'Ruby on Rails') }
   scope :angular_items, -> { where(subtitle: 'Angular') }
 
-  before_validation :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 end
