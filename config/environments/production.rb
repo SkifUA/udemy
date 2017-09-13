9e75aa40f233ef7692f68c34790286d769e0fea3
Valerii Kuzin <kuzin.valeriy@gmail.com>
2017-09-13 22:09:10 +0300

    Customize mailer for send mail reset password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,24 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "udemy_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_url_options = { :host => ENV['PROJECT_HOST'] }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+
+      user_name: ENV['MAILER_USER_NAME'],
+      password: ENV['MAILER_PASSWORD'],
+      address: ENV['MAILER_ADDRESS'],
+      domain: "gmail.com",
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
